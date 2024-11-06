22af82a79345ed7db2bcff022c57b77907c7f580
Corentin Arnaud <corentin.arnaud45@gmail.com>
2024-11-07 00:45:06 +0100

    microbench class features
    Adding methods and attributes to microbench + a test class called in main

--- include/PPN-microbench/microbench.hpp
+++ include/PPN-microbench/microbench.hpp
@@ -1,21 +1,30 @@
 #ifndef MICROBENCH
 #define MICROBENCH
 
+#include <vector>
+#include <any>
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
 
 class Microbench {
+    private:
+        std::vector<std::any> results;
+        std::string name;
+        //std::string category;
+        int nbIterations;
 
-private:
-    
-    
-public:
-    Microbench();
-    ~Microbench();
+        virtual void executeBench() = 0;
 
-    virtual bool executeBench() = 0;
-    virtual json getJson() = 0;
+    public:
+        Microbench(std::string name, int nbIterations);
+        ~Microbench();
+
+        std::string getName();
+        int getNbIterations();
+        
+        void run();
+        virtual json getJson() = 0;
 };
 
 
--- src/PPN-microbench/microbench.cpp
+++ src/PPN-microbench/microbench.cpp
@@ -1,9 +1,23 @@
 #include <PPN-microbench/microbench.hpp>
 
-Microbench::Microbench() {
-
+Microbench::Microbench(std::string name, int nbIterations) {
+    this->name = name;
+    this->nbIterations = nbIterations;
 }
 
 Microbench::~Microbench() {
+}
 
+void Microbench::run() {
+    for (int i = 0; i < this->nbIterations; i++) {
+        executeBench();
+    }
+}
+
+std::string Microbench::getName(){
+    return this->name;
+}
+
+int Microbench::getNbIterations(){
+    return this->nbIterations;
 }
