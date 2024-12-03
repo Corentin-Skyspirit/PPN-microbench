bdc93491307d09db75c399c298718ddc2eef4132
Gautier <Hybanos@orange.fr>
2024-12-03 17:20:13 +0100

    res size_t** -> static 2d array, kernel changes

--- include/PPN-microbench/ops.hpp
+++ include/PPN-microbench/ops.hpp
@@ -1,23 +1,23 @@
-#ifndef _OPS_HPP
-#define _OPS_HPP
+#ifndef PPN_MICROBENCH_OPS_HPP
+#define PPN_MICROBENCH_OPS_HPP
 
 #include <PPN-microbench/microbench.hpp>
 
 #include <chrono>
 #include <thread>
-// needed to set thread affinty
-#include <threads.h>
-// clock
-#include <time.h>
+#include <threads.h> // needed to set thread affinty
+#include <time.h>    // clock
 
 class Ops : public Microbench {
   private:
-    size_t const N_OPS = 1024 * 1024 * 16;
+    int static const RUNS = 10;
+
+    size_t const N_OPS = 1024 * 1024 * 8;
     size_t cpus;
 
-    size_t **results;
+    size_t results[6][RUNS];
 
-    template <class T> void bench(T);
+    template <class T> void benchhaha(T);
     template <class T> void benchSIMD(T);
     void executeBench() {};
 
