9013bc2981fccdbd7deacb712a85f931d19aa6ed
Corentin Arnaud <corentin.arnaud45@gmail.com>
2025-02-25 19:05:43 +0100

    my bad I've missnamed my class :/

---
+++ include/PPN-microbench/core_to_core_latency.hpp
@@ -0,0 +1,22 @@
+#ifndef PPN_MICROBENCH_CORE_TO_CORE_LATENCY
+#define PPN_MICROBENCH_CORE_TO_CORE_LATENCY
+
+#include <PPN-microbench/microbench.hpp>
+#include <PPN-microbench/context.hpp>
+
+#include <chrono>
+#include <thread>
+
+class CoreToCoreLatency : public Microbench {
+    private:
+        int nbCores;
+        std::vector<int> results;
+    public:
+        CoreToCoreLatency(int nbMeasures);
+        ~CoreToCoreLatency();
+
+        void run() override;
+        json getJson() override;
+};
+
+#endif
---
+++ src/PPN-microbench/core_to_core_latency.cpp
@@ -0,0 +1,51 @@
+#include <PPN-microbench/core_to_core_latency.hpp>
+
+using std::chrono::duration_cast;
+using std::chrono::nanoseconds;
+using std::chrono::steady_clock;
+
+CoreToCoreLatency::CoreToCoreLatency(int nbMeasures) : Microbench("CoreToCoreLatency", 10) {
+    nbCores = context.getCpus();
+}
+
+CoreToCoreLatency::~CoreToCoreLatency() {}
+
+void CoreToCoreLatency::run() {
+    Context context = Context::getInstance();
+    std::vector<size_t> threadMapping = context.getThreadMapping();
+
+    cpu_set_t cpusets[nbCores];
+
+    for (int i = 0; i < nbCores; i++) {
+        CPU_ZERO(&cpusets[i]);
+        CPU_SET(threadMapping[i], &cpusets[i]);
+    }
+
+    std::thread threads[nbCores];
+
+    for (int id_1 = 0; id_1 < nbCores; id_1++) {
+        for (int id_2 = 0; id_2 < nbCores; id_2++) {
+            if (id_1 == id_2) {
+                results.push_back(0);
+            } else {
+                auto start = steady_clock::now();
+
+                u64 duration = duration_cast<nanoseconds>(steady_clock::now() - start).count();
+                results.push_back(duration);
+                std::cout << "\n" << id_1 << " " << id_2 << " " << duration << std::endl;
+            }
+            std::cout << "\r# " << name << ": run " << id_1 * nbCores + (id_2 + 1) << "/" << nbCores * nbCores << std::flush;
+        }
+    }
+}
+
+json CoreToCoreLatency::getJson() { 
+    json coreToCoreJson = json::object();
+    coreToCoreJson["name"] = getName();
+    for (int line = 0; line < nbCores; line++) {
+        for (int col = 0; col < nbCores; col++) {
+            coreToCoreJson["results"][line] += results[line * nbCores + col];
+        }
+    }
+    return coreToCoreJson;
+}
