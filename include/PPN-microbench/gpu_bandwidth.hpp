440b97c55e3ee745e4099b5d68013c407820739c
Gautier <Hybanos@orange.fr>
2025-04-12 23:21:05 +0200

    hip hip hip

--- include/PPN-microbench/gpu_bandwidth.hpp
+++ include/PPN-microbench/gpu_bandwidth.hpp
@@ -4,19 +4,30 @@
 #include <PPN-microbench/microbench.hpp>
 #include <PPN-microbench/context.hpp>
 
-#include <chrono>
-#include <thread>
-
 #include <hip/hip_runtime.h>
 #include <hip/hip_runtime_api.h>
 
+#include <chrono>
+#include <thread>
+
+#define GPUERR(f) if (gpuerr(f)) {return;}
+
 class GPUBandwidth : public Microbench {
     private:
-        uint64_t max_buffer_size;
+        uint64_t const MAX_SIZE = 1 << 30;
+
+        uint64_t meta[21];
+        uint64_t data[5][21];
+
+        // used to provide info in reports if bench execution doesn't go as planned.
+        std::string bench_info = "";
+
+        bool gpuerr(hipError_t err);
         bool gpuprep();
         void _run();
     public:
         GPUBandwidth();
+        ~GPUBandwidth() = default;
 
         void run() override;
         json getJson() override;
--- src/PPN-microbench/gpu/gpu_bandwidth.cpp
+++ src/PPN-microbench/gpu/gpu_bandwidth.cpp
@@ -5,21 +5,32 @@ using std::chrono::time_point;
 
 GPUBandwidth::GPUBandwidth() : Microbench("GPU_Bandwidth", 5) {}
 
+bool GPUBandwidth::gpuerr(hipError_t err) {
+    if (err != hipSuccess) {
+        spdlog::error("hip error: {}", hipGetErrorString(err));
+        spdlog::error("cancelling bench...");
+        bench_info = "Bench returned the following HIP error: " + std::string(hipGetErrorString(err));
+        return true;
+    }
+    return false;
+}
+
 bool GPUBandwidth::gpuprep() {
     int n_devices;
-    hipGetDeviceCount(&n_devices);
+    hipError_t err;
+    err = hipGetDeviceCount(&n_devices);
     if (!n_devices) {
         spdlog::warn("No hip capable device, skipping !");
+        bench_info = "Could not find any hip capable device, benchmark is skipped.";
         return false;
     }
 
     spdlog::info("Number of HIP devices {}", n_devices);
     int device;
     hipDeviceProp_t props;
-    hipGetDeviceProperties(&props, 0);
+    err = hipGetDeviceProperties(&props, 0);
     spdlog::info("Device 0 name: {}", props.name);
-    spdlog::info("Device 0 memory: {}", props.totalGlobalMem);
-    max_buffer_size = props.totalGlobalMem;
+    spdlog::info("Device 0 memory: {} Bytes", props.totalGlobalMem);
 
     return true;
 }
@@ -27,18 +38,43 @@ bool GPUBandwidth::gpuprep() {
 void GPUBandwidth::_run() {
     char *buff;
     char *array;
-    uint64_t size = 1 << 30;
     time_point<high_resolution_clock> t1, t2;
 
-    array = (char *) calloc(size, sizeof(char));
-    
-    hipMalloc(&buff, size);
-    t1 = high_resolution_clock::now();
-    hipMemcpy(buff, array, size, hipMemcpyHostToDevice);
-    hipDeviceSynchronize();
-    t2 = high_resolution_clock::now();
+    array = new char[MAX_SIZE];
+    spdlog::debug("Touching data...");
+    for (uint64_t i = 0; i < MAX_SIZE; i++) {
+        array[i] = rand();
+    }
+    spdlog::debug("Done!");
 
-    spdlog::info("{}", (t2 - t1).count() / 1000000);
+    GPUERR(hipMalloc(&buff, MAX_SIZE))
+
+    spdlog::debug("Warming up...");
+    for (int i = 0; i < 500; i++) {
+        GPUERR(hipMemcpy(buff, array, MAX_SIZE, hipMemcpyHostToDevice))
+        GPUERR(hipDeviceSynchronize())
+    }
+
+    for (int rep = 0; rep < nbIterations; rep++) {
+        spdlog::debug("run {}/{}", rep+1, nbIterations);
+        int cpt = 0;
+        for (size_t size = 1024; size <= MAX_SIZE; size *= 2) {
+            t1 = high_resolution_clock::now();
+            for (int i = 0; i < 1000; i++) {
+                GPUERR(hipMemcpy(buff, array, size, hipMemcpyHostToDevice))
+                GPUERR(hipDeviceSynchronize())
+            }
+            t2 = high_resolution_clock::now();
+            
+            uint64_t time = (t2 - t1).count() / 1000;
+            spdlog::debug("size: {}MiB, {}ms, {}GB/s", (float)size / (1<<20), time / 1e6, (float)size / time);
+            meta[cpt] = size;
+            data[rep][cpt] = time;
+            cpt++;
+        }
+    }
+
+    delete[] array;
 }
 
 void GPUBandwidth::run() {
@@ -46,6 +82,16 @@ void GPUBandwidth::run() {
 }
 
 json GPUBandwidth::getJson() {
-    json j;
-    return j;
+    json obj;
+    obj["name"] = name;
+    obj["info"] = bench_info;
+
+    for (int rep = 0; rep < nbIterations; rep++) {
+        for (int cpt = 0; cpt <= 20; cpt++) {
+            obj["meta"][cpt] = meta[cpt];
+            obj["data"][rep][cpt] = data[rep][cpt];
+        }
+    }
+
+    return obj;
 }
