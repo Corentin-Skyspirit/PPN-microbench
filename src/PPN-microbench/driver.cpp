e5e6b0703e32886b832b0dcf60d1fe97ad8b3e9c
A-Tolan <andreea.tolan@ens.uvsq.fr>
2025-04-28 00:34:36 +0200

    update driver.cpp : update command line flag for matrix multiplication from --matrix to --matrix-mult for clarity

--- src/PPN-microbench/driver.cpp
+++ src/PPN-microbench/driver.cpp
@@ -15,7 +15,7 @@ Driver::Driver(int argc, char **argv) {
     app.add_flag_callback("--ops", [this](){this->addBench(new Ops(10));}, "Run operations/second benchmark");
     app.add_flag_callback("--c2c", [this](){this->addBench(new CoreToCoreLatency(10));}, "Run core to core latency benchmark");
     app.add_flag_callback("--cache-latency", [this](){this->addBench(new Cache_latency);}, "Run cpu ram/cache latency benchmark");
-    app.add_flag_callback("--matrix", [this](){this->addBench(new Matrix_bench);}, "Run matrix multiplication benchmark");
+    app.add_flag_callback("--matrix-mult", [this](){this->addBench(new Matrix_bench);}, "Run matrix multiplication benchmark");
     // benchmark group selection
     app.add_flag_callback("--cpu", [this](){this->addBench(new CPUFrequency(10)).addBench(new Ops(10)).addBench(new CoreToCoreLatency(10));}, "CPU related benchmarks");
     app.add_flag_callback("--mem", [this](){this->addBench(new Cache_latency);}, "Memory/cache related benchmarks");
